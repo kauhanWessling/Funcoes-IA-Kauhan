b3d99d721b4ad974ab08a3299f475cbbd65d4d74
kauhanWessling <108003319+kauhanWessling@users.noreply.github.com>
2024-08-16 10:50:02 -0300

    16/08/2024

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-
-
-
-
+Const caixaPrincipal = document.querySelector(".caixa-principal");
+Const caixaPergunta = document.querySelector(".caixa-pergunta");
+Const caixaAlternativa = document.querySelector(".caixa-Alternativa");
+Const caixaResultado = document.querySelector(".caixa-Resultado");
+const textoResultado =
 
 const perguntas = [
     {
